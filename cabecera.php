f959ec5c6c04ab5bf2436e488f2ccccd3fb80960
julialujan666 <39529606+julialujan666@users.noreply.github.com>
2018-05-22 18:20:05 +0200

    Add files via upload

---
+++ cabecera.php
@@ -0,0 +1,18 @@
+
+  <!--NAVEGADOR-->
+  <header>
+  <nav  style="background-color:#70724D;" >
+  <div class="container-fluid">
+    <div class="navbar-header">
+       <h4 class=" color">TENIS</h4>
+    </div>
+    <ul class="nav navbar-nav">
+      <li><a href="http://localhost/codeigniter/index.php/tenis">El tenis</a></li>
+      <li><a href="http://localhost/codeigniter/index.php/tenis/galeria">Galeria</a></li>
+      <li><a href="#">Quienes somos</a></li>
+      <li><a href="#">Usuario</a></li>
+       <li><a href="#">Nuevo usuario</a></li>
+    </ul>
+  </div>
+</nav>
+</header>
